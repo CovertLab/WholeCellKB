0360b0dc4489bdf24a6bdb1c8641b8a2095b55db
Jonathan Karr <jonrkarr@gmail.com>
2013-05-27 15:03:03 -0700

    implementing view parameters in simulation code

--- public/views.py
+++ public/views.py
@@ -29,12 +29,10 @@ from public import models
 from public.forms import ExportDataForm, ImportDataForm
 from public.helpers import getEntry, format_field_detail_view, objectToQuerySet, render_queryset_to_response, getObjectTypes, getModel, get_invalid_objects, get_edit_form_fields, get_edit_form_data
 from public.helpers import validate_object_fields, validate_model_objects, validate_model_unique, save_object_data, batch_import_from_excel, readFasta
-from public.models import Entry, Species, SpeciesComponent, Reference, ModelProperty, SimulationProperty
+from public.helpers import PropertyDefinitionFilter, PygmentsStyle, PygmentsFormatter
+from public.models import Entry, Parameter, Reference, Species, SpeciesComponent, ModelProperty, SimulationProperty
 import pygments
-from pygments.filter import Filter
-from pygments.formatters import HtmlFormatter
 from pygments.lexers import MatlabLexer
-from pygments.style import Style
 from pygments.token import Token
 from urlparse import urlparse
 import numpy
@@ -535,8 +533,9 @@ def detail(request, species_wid, wid):
 		rmfields = []
 		for idx2 in range(len(fieldsets[idx][1]['fields'])):
 			if isinstance(fieldsets[idx][1]['fields'][idx2], dict):
-				field_name = fieldsets[idx][1]['fields'][idx2]['name']
-				verbose_name = fieldsets[idx][1]['fields'][idx2]['verbose_name']
+				field = fieldsets[idx][1]['fields'][idx2]
+				field_name = field['name']
+				verbose_name = field['verbose_name']
 			else:
 				field_name = fieldsets[idx][1]['fields'][idx2]
 				field = model._meta.get_field_by_name(field_name)[0]
@@ -549,20 +548,33 @@ def detail(request, species_wid, wid):
 			if (data is None) or (data == ''):
 				rmfields = [idx2] + rmfields
 				
-			try:
-				is_modeled = ModelProperty.objects.get(
-					species__wid = species_wid,
-					class_name = model_type,
-					property_name = field_name
-					).simulation_properties.exists()
-			except ObjectDoesNotExist:
-				is_modeled = False
-				
+			if issubclass(model, Parameter) and field_name == 'value':
+				is_modeled_url = reverse('public.views.viewParameterInSimulation', kwargs = {
+					'species_wid':species_wid, 
+					'wid': wid,
+					})
+			elif (isinstance(field, (ForeignKey, ManyToManyField)) and issubclass(field.rel.to, Parameter)) or \
+			   (isinstance(field, (RelatedObject)) and issubclass(field.model, Parameter)):
+				is_modeled_url = reverse('public.views.viewParametersInSimulation', kwargs = {
+					'species_wid':species_wid, 
+					'wid': wid,
+					})
+			else:
+				try:
+					if ModelProperty.objects.get(species__wid = species_wid, class_name = model_type, property_name = field_name).simulation_properties.exists():
+						is_modeled_url = reverse('public.views.viewPropertyInSimulation', kwargs = {
+							'species_wid':species_wid, 
+							'class_name': model_type, 
+							'property_name': field_name,
+							})
+				except ObjectDoesNotExist:
+					is_modeled_url = ''
+			
 			fieldsets[idx][1]['fields'][idx2] = {
 				'name': field_name,
 				'verbose_name': verbose_name.replace(" ", '&nbsp;').replace("-", "&#8209;"), 
 				'data': data,
-				'is_modeled': is_modeled,
+				'is_modeled_url': is_modeled_url,
 				}
 		for idx2 in rmfields:
 			del fieldsets[idx][1]['fields'][idx2]
@@ -655,175 +667,129 @@ def viewPropertyInSimulation(request, species_wid, class_name, property_name):
 		templateFile = 'public/viewPropertyInSimulation.html', 
 		data = {
 			'object_list': object_list,
-			'class_name': class_name,
-			'property_name': property_name,
 			'verbose_class_name': verbose_class_name,
 			'verbose_property_name': verbose_property_name,
 			})
+			
+def viewParameterInSimulation(request, species_wid, wid):
+	#get associated simulation property
+	qs = Parameter.objects.filter(species__wid = species_wid, wid=wid)
+	if not qs[0].state is None:
+		sim_class_name = 'edu.stanford.covert.cell.sim.state.%s' % qs[0].state.wid.replace('State_', '')
+		verbose_class_name = '%s: %s' % (wid, qs[0].state.name)
+	else:
+		sim_class_name = 'edu.stanford.covert.cell.sim.process.%s' % qs[0].process.wid.replace('Process_', '')
+		verbose_class_name = '%s: %s' % (wid, qs[0].process.name)
+	sim_property_name = qs[0].name
+	verbose_property_name = qs[0].name
 
-
-class PropertyDefinitionFilter(Filter):
-	def __init__(self, **options):
-		Filter.__init__(self, **options)
-		self.property_names = options.get('property_names')
-		self.tokentype = options.get('tokentype')
+	#highlight code for simulation class
+	pathParts = sim_class_name.split('.')
+	codePath = "%s/src/+%s/%s.m" % (MODEL_CODE_BASE_DIR, '/+'.join(pathParts[0:-1]), pathParts[-1])
+	if not os.path.isfile(codePath):
+		codePath = "%s/src/+%s/@%s/%s.m" % (MODEL_CODE_BASE_DIR, '/+'.join(pathParts[0:-1]), pathParts[-1], pathParts[-1])
 	
-	def filter(self, lexer, stream):
-		for ttype, value in stream:
-			if value in self.property_names:
-				ttype = self.tokentype
-			yield ttype, value
+	if os.path.isfile(codePath):
+		with open (codePath, "r") as codeFile:
+			code = codeFile.read()
 
-class PygmentsStyle(Style):
-	default_style = ""
-	styles = {
-		Token.Comment:                'italic #999',
-		Token.Keyword:                'bold #0000FF',
-		Token.Name.Variable:          'bold #f00',
-		Token.Name.Function:          '#228B22',
-    }
-	
-class PygmentsFormatter(HtmlFormatter):
-	def __init__(self, **options):
-		HtmlFormatter.__init__(self, **options)
+		lexer = MatlabLexer()
+		lexer.add_filter(PropertyDefinitionFilter(property_names = [sim_property_name], tokentype=Token.Name.Variable)) 
 		
-	def _wrap_inlinelinenos(self, inner):
-		# need a list of lines since we need the width of a single number :(
-		lines = inner
-		st = self.linenostep
-		num = self.linenostart
-
-		for t, line in lines:
-			yield 1, ('<span style="color: #ccc; '
-					  'padding: 0 5px 0 5px">%4d</span> ' % (
-					  num) + line)
-			num += 1
+		tokens = lexer.get_tokens(code)
+			
+		object = {
+			'class_name': sim_class_name,
+			'property_names': [sim_property_name],
+			'code': pygments.format(tokens, PygmentsFormatter(linenos='inline', linenostep=1, style=PygmentsStyle, noclasses=True)),
+			}
+	else:
+		raise Http404
 	
-	def _format_lines(self, tokensource):
-		"""
-		Just format the tokens, without any wrapping tags.
-		Yield individual lines.
-		"""
-		nocls = self.noclasses
-		lsep = self.lineseparator
-		# for <span style=""> lookup only
-		getcls = self.ttype2class.get
-		c2s = self.class2style
-		escape_table = pygments.formatters.html._escape_html_table
-		tagsfile = self.tagsfile
+	#render response
+	return render_queryset_to_response(
+		species_wid = species_wid,		
+		request = request, 
+		models = [Parameter],
+		queryset = qs,
+		templateFile = 'public/viewPropertyInSimulation.html', 
+		data = {
+			'object_list': [object],
+			'verbose_class_name': verbose_class_name,
+			'verbose_property_name': verbose_property_name,
+			})
+	
+def viewParametersInSimulation(request, species_wid, wid):
+	obj = getEntry(species_wid = species_wid, wid = wid)
+	if obj is None:
+		raise Http404
+		
+	model = obj.__class__
+	verbose_class_name = model._meta.verbose_name
+	verbose_property_name = '%s parameters' % obj.name
+	
+	#get associated simulation property
+	qs = EmptyQuerySet()
+	for field in model._meta.get_all_related_objects() + model._meta.get_all_related_many_to_many_objects():
+		accessor_name = field.get_accessor_name()
+		if accessor_name == '+':
+			continue
 
-		lspan = ''
-		line = ''
-		is_highlight_line = False
-		for ttype, value in tokensource:
-			is_highlight_line = is_highlight_line or ttype == Token.Name.Variable
+		if isinstance(field, ForeignKey) and issubclass(field.rel.to, Parameter):
+			qs._result_cache.append(getattr(obj, accessor_name))
+		if (isinstance(field, ManyToManyField) and issubclass(field.rel.to, Parameter)) or \
+		   (isinstance(field, RelatedObject) and issubclass(field.model, Parameter)):
+			qs2 = getattr(obj, accessor_name).all()
+			if len(qs2) > 0:
+				qs._result_cache.extend(qs2._result_cache)
+	
+	classes = {}	
+	for o in qs:
+		if not o.state is None:
+			sim_class_name = 'edu.stanford.covert.cell.sim.state.%s' % o.state.wid.replace('State_', '')
+		else:
+			sim_class_name = 'edu.stanford.covert.cell.sim.process.%s' % o.process.wid.replace('Process_', '')
+		if not classes.has_key(sim_class_name):
+			classes[sim_class_name] = []			
+		classes[sim_class_name].append(o.name)
+
+	#highlight code for simulation class
+	objects = []
+	for sim_class_name in classes:
+		sim_property_names = classes[sim_class_name]
+		sim_property_names.sort()
+		pathParts = sim_class_name.split('.')
+		codePath = "%s/src/+%s/%s.m" % (MODEL_CODE_BASE_DIR, '/+'.join(pathParts[0:-1]), pathParts[-1])
+		if not os.path.isfile(codePath):
+			codePath = "%s/src/+%s/@%s/%s.m" % (MODEL_CODE_BASE_DIR, '/+'.join(pathParts[0:-1]), pathParts[-1], pathParts[-1])
+			if not os.path.isfile(codePath):
+				continue
+		with open (codePath, "r") as codeFile:
+			code = codeFile.read()
+
+		lexer = MatlabLexer()
+		lexer.add_filter(PropertyDefinitionFilter(property_names = sim_property_names, tokentype=Token.Name.Variable)) 
+		
+		tokens = lexer.get_tokens(code)
 			
-			if nocls:
-				cclass = getcls(ttype)
-				while cclass is None:
-					ttype = ttype.parent
-					cclass = getcls(ttype)
-				cspan = cclass and '<span style="%s">' % c2s[cclass][0] or ''
-			else:
-				cls = self._get_css_class(ttype)
-				cspan = cls and '<span class="%s">' % cls or ''
-
-			parts = value.translate(escape_table).split('\n')
-
-			if tagsfile and ttype in Token.Name:
-				filename, linenumber = self._lookup_ctag(value)
-				if linenumber:
-					base, filename = os.path.split(filename)
-					if base:
-						base += '/'
-					filename, extension = os.path.splitext(filename)
-					url = self.tagurlformat % {'path': base, 'fname': filename,
-											   'fext': extension}
-					parts[0] = "<a href=\"%s#%s-%d\">%s" % \
-						(url, self.lineanchors, linenumber, parts[0])
-					parts[-1] = parts[-1] + "</a>"
-
-			# for all but the last line
-			for part in parts[:-1]:
-				if line:
-					if lspan != cspan:
-						line += (lspan and '</span>') + cspan + part + \
-								(cspan and '</span>') + lsep
-					else: # both are the same
-						line += part + (lspan and '</span>') + lsep
-					yield is_highlight_line + 1, line
-					line = ''
-					is_highlight_line = False
-				elif part:
-					yield is_highlight_line + 1, cspan + part + (cspan and '</span>') + lsep
-				else:
-					yield is_highlight_line + 1, lsep
-			# for the last line
-			if line and parts[-1]:
-				if lspan != cspan:
-					line += (lspan and '</span>') + cspan + parts[-1]
-					lspan = cspan
-				else:
-					line += parts[-1]
-			elif parts[-1]:
-				line = cspan + parts[-1]
-				lspan = cspan
-			# else we neither have to open a new span nor set lspan
-
-		if line:
-			yield is_highlight_line + 1, line + (lspan and '</span>') + lsep
-			
-	def _highlight_lines(self, tokensource):
-		"""
-		Highlighted the lines specified in the `hl_lines` option by
-		post-processing the token stream coming from `_format_lines`.
-		"""
-		hls = self.hl_lines
-
-		for i, (t, value) in enumerate(tokensource):
-			if t > 1 or i + 1 in hls: # i + 1 because Python indexes start at 0
-				if self.noclasses:
-					style = ''
-					if self.style.highlight_color is not None:
-						style = (' style="background-color: %s"' %
-								 (self.style.highlight_color,))
-					yield 1, '<span%s>%s</span>' % (style, value)
-				else:
-					yield 1, '<span class="hll">%s</span>' % value
-			else:
-				yield 1, value
-				
-	def format_unencoded(self, tokensource, outfile):
-		"""
-		The formatting process uses several nested generators; which of
-		them are used is determined by the user's options.
-
-		Each generator should take at least one argument, ``inner``,
-		and wrap the pieces of text generated by this.
-
-		Always yield 2-tuples: (code, text). If "code" is 1, the text
-		is part of the original tokensource being highlighted, if it's
-		0, the text is some piece of wrapping. This makes it possible to
-		use several different wrappers that process the original source
-		linewise, e.g. line number generators.
-		"""
-		source = self._format_lines(tokensource)
-		source = self._highlight_lines(source)
-		if not self.nowrap:
-			if self.linenos == 2:
-				source = self._wrap_inlinelinenos(source)
-			if self.lineanchors:
-				source = self._wrap_lineanchors(source)
-			if self.linespans:
-				source = self._wrap_linespans(source)
-			source = self.wrap(source, outfile)
-			if self.linenos == 1:
-				source = self._wrap_tablelinenos(source)
-			if self.full:
-				source = self._wrap_full(source, outfile)
-
-		for t, piece in source:
-			outfile.write(piece)
+		objects.append({
+			'class_name': sim_class_name,
+			'property_names': sim_property_names,
+			'code': pygments.format(tokens, PygmentsFormatter(linenos='inline', linenostep=1, style=PygmentsStyle, noclasses=True)),
+			})
+	
+	#render response
+	return render_queryset_to_response(
+		species_wid = species_wid,		
+		request = request, 
+		models = [Parameter],
+		queryset = qs,
+		templateFile = 'public/viewPropertyInSimulation.html', 
+		data = {
+			'object_list': objects,
+			'verbose_class_name': verbose_class_name,
+			'verbose_property_name': verbose_property_name,
+			})
 			
 @login_required		
 def add(request, model_type, species_wid=None):
